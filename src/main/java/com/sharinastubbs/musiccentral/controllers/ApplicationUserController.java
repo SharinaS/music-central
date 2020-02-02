494e8a283a34623ef14ce2d4f4e9c8717cda3bd4
Sharina Stubbs <sharinastubbs@gmail.com>
2020-02-02 15:25:15 -0800

    added functionality for user to signup, login, logout, with appropriate error messages for the user. Added bootstrap to head fragment and styled login and signup forms with bootstrap. Added nav fragment that differentiates between a logged in versus non-logged in user. Added a couple user stories.

--- src/main/java/com/sharinastubbs/musiccentral/controllers/ApplicationUserController.java
+++ src/main/java/com/sharinastubbs/musiccentral/controllers/ApplicationUserController.java
@@ -1,5 +1,56 @@
 package com.sharinastubbs.musiccentral.controllers;
 
+import com.sharinastubbs.musiccentral.models.ApplicationUser;
+import com.sharinastubbs.musiccentral.models.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.ArrayList;
+
+@Controller
 public class ApplicationUserController {
-    //TODO: Flesh out this class, so a person can make an account and login in, and access the home page.
+    // Connection to user repository
+    @Autowired
+    ApplicationUserRepository applicationUserRepository;
+
+    // (bean added to WebSecurityConfig to allow for encoder)
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    //== GET routes for sign up page and login page ==
+    @GetMapping("/signup")
+    public String goToSignUpPage() {
+        return "signup";
+    }
+
+    @GetMapping("/login")
+    public String goToLoginPage() {
+        return "login";
+    }
+
+    //== POST route for form data from account signup ==
+    @PostMapping("/signup")
+    public RedirectView signupForAccount(String username, String password, String firstname) {
+        if (applicationUserRepository.findByUsername(username) == null) {
+            // create and save a new user and let them stay logged in
+            ApplicationUser newUser = new ApplicationUser(username, passwordEncoder.encode(password), firstname);
+            applicationUserRepository.save(newUser);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(newUser,
+                    null,
+                    new ArrayList<>());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            //TODO: redirect over to albums page
+            return new RedirectView("/");
+        } else {
+            // user needs to choose a different username. Note that taken is the param.
+            return new RedirectView("/signup?taken=true");
+        }
+    }
 }
