7dea0ac1008680a199ffbbd985b47e028db70381
Sharina Stubbs <sharinastubbs@gmail.com>
2020-02-07 16:21:46 -0800

    wired up database with one to many relationship of user to artists, added Artist class and ArtistRepository, added view for artists and updated nav page with artists

---
+++ src/main/java/com/sharinastubbs/musiccentral/models/ArtistRepository.java
@@ -0,0 +1,6 @@
+package com.sharinastubbs.musiccentral.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArtistRepository extends JpaRepository<Artist, Long> {
+}
