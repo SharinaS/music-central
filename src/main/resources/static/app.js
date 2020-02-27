30669746671989493f9ca5c1a68723d8e37a061b
Sharina Stubbs <sharinastubbs@gmail.com>
2020-02-26 21:13:37 -0800

    JS popup box now appears when delete button clicked

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,16 +1,21 @@
 'use strict';
 
 $(function() {
-  // removes artist or song info, and replaces with text fields to fill out
+  // removes artist or song info, and replaces with form for user to submit info
   $('.edit-button').click(function(){
     console.log('clicked on edit button');
-//    $(this).parent().parent('text-to-hide').hide();
-//    $('.form-to-show').removeAttr('hidden');
-
     $(this).parent().parent().find('.form-to-show').removeAttr('hidden');
     $(this).parent().parent().find('.text-to-hide').hide();
+   });
 
+   // Pop up box when delete button clicked
+   $('.delete-button').click(function(event){
+    console.log('clicked on delete button');
+    if(confirm("Are you sure you want to DELETE?")) {
+        console.log("OK chosen");
+    } else {
+        console.log("cancel chosen");
+        event.preventDefault();
+    }
    });
 })
-
-//TODO: Make alert that confirms user wants to delete the artist or song
