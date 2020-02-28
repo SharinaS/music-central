e1a8a60a90ee41ec71399650b5a84e8f5c607399
Sharina Stubbs <sharinastubbs@gmail.com>
2020-02-28 08:00:17 -0800

    delete button functionality improved for artists and songs

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,21 +1,31 @@
 'use strict';
 
 $(function() {
-  // removes artist or song info, and replaces with form for user to submit info
+  // === Edit functionality - removes artist or song info, and replaces with hidden form ===
   $('.edit-button').click(function(){
     console.log('clicked on edit button');
     $(this).parent().parent().find('.form-to-show').removeAttr('hidden');
     $(this).parent().parent().find('.text-to-hide').hide();
    });
 
-   // Pop up box when delete button clicked
-   $('.delete-button').click(function(event){
+   // === Pop-up boxes for delete buttons ===
+   $('.delete-song').click(function(event){
     console.log('clicked on delete button');
-    if(confirm("Are you sure you want to DELETE?")) {
+    if(confirm("Are you sure you want to DELETE this song?")) {
         console.log("OK chosen");
     } else {
         console.log("cancel chosen");
         event.preventDefault();
     }
    });
+
+      $('.delete-artist').click(function(event){
+       console.log('clicked on delete button');
+       if(confirm("Are you sure you want to DELETE this artist? All songs associated with this artist will also be deleted!")) {
+           console.log("OK chosen");
+       } else {
+           console.log("cancel chosen");
+           event.preventDefault();
+       }
+      });
 })
